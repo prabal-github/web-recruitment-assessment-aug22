e2d552ef7bbc247803be56aac67c944abf359ce6
prabal-github <prabal.chow09009.pc@gmail.com>
2022-08-01 21:36:45 +0530

    Complete 3

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
     var count = questionNumber;
     setquestionNumber(count + 1);
 
-    console.log(questions);
+    // console.log(questions);
 
     for (let i = 0; i < questions.length; i++) {
       optns[i] = questions[i].incorrect_answers;
@@ -70,16 +70,16 @@ function App() {
       }
     }
     setnewoptions(optns)
-    console.log(newoptions);
-    console.log(started);
-    console.log(showScore);
+    // console.log(newoptions);
+    // console.log(started);
+    // console.log(showScore);
   }
 
   const nextquestion = () => {
     var count = questionNumber;
     (questionNumber < 4) ? setquestionNumber(count + 1) : setShowScore(true);
-    console.log(selectedOption);
-    console.log(questions[questionNumber].correct_answer);
+    // console.log(selectedOption);
+    // console.log(questions[questionNumber].correct_answer);
     if (questions[questionNumber].correct_answer === selectedOption) {
       var currentScore = score;
       setscore(currentScore + 1);
@@ -89,7 +89,7 @@ function App() {
   const reset = () => {
     setscore(0);
     setstarted(false)
-    console.log('reset successfull');
+    // console.log('reset successfull');
     setShowScore(false)
     const resetUseeffect = start;
     setstart(resetUseeffect ? false : true);
@@ -153,34 +153,33 @@ function App() {
                               m-0
                               focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   aria-label="Default select example"
-                >
-                                          
+                >                                          
                   <option selected disabled>Select Category</option>
-                  <option value="any">any</option>
-                  <option value="general">general</option>
-                  <option value="books">books</option>
-                  <option value="film">film</option>
-                  <option value="music">music</option>
-                  <option value="theatre">theatre</option>
-                  <option value="television">television</option>
-                  <option value="videogames">videogames</option>
-                  <option value="boardgames">boardgames</option>
-                  <option value="science">science</option>
-                  <option value="computers">computers</option>
-                  <option value="mathematics">mathematics</option>
-                  <option value="mythology">mythology</option>
-                  <option value="sports">sports</option>
-                  <option value="geography">geography</option>
-                  <option value="history">history</option>
-                  <option value="politics">politics</option>
-                  <option value="art">art</option>
-                  <option value="celebrities">celebrities</option>
-                  <option value="animals">animals</option>
-                  <option value="vehicles">vehicles</option>
-                  <option value="comics">comics</option>
-                  <option value="gadgets">gadgets</option>
-                  <option value="anime">anime</option>
-                  <option value="cartoons">cartoons</option>
+                  <option value="any">Any</option>
+                  <option value="general">General</option>
+                  <option value="books">Books</option>
+                  <option value="film">Film</option>
+                  <option value="music">Music</option>
+                  <option value="theatre">Theatre</option>
+                  <option value="television">Television</option>
+                  <option value="videogames">Video Games</option>
+                  <option value="boardgames">Board Games</option>
+                  <option value="science">Science</option>
+                  <option value="computers">Computers</option>
+                  <option value="mathematics">Mathematics</option>
+                  <option value="mythology">Mythology</option>
+                  <option value="sports">Sports</option>
+                  <option value="geography">Geography</option>
+                  <option value="history">History</option>
+                  <option value="politics">Politics</option>
+                  <option value="art">Art</option>
+                  <option value="celebrities">Celebrities</option>
+                  <option value="animals">Animals</option>
+                  <option value="vehicles">Vehicles</option>
+                  <option value="comics">Comics</option>
+                  <option value="gadgets">Gadgets</option>
+                  <option value="anime">Anime</option>
+                  <option value="cartoons">Cartoons</option>
                 </select>
               </div>
 
@@ -188,7 +187,7 @@ function App() {
                 <button className="w-full text-white font-bold text-md uppercase bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 rounded-lg px-5 py-2.5 text-center mr-2 mb-2" onClick={startquestioncount}>
                   Start
                 </button> :
-                <button className="text-white font-bold text-md uppercase bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 rounded-lg px-5 py-2.5 text-center mr-2 mb-2">
+                <button className="w-full text-white font-bold text-md uppercase bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 rounded-lg px-5 py-2.5 text-center mr-2 mb-2">
                   <svg aria-hidden="true" role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
