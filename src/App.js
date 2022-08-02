8e4d27d0d2f9cc645dbbd2405f0f5c2081628e61
prabal-github <prabal.chow09009.pc@gmail.com>
2022-08-02 12:46:40 +0530

    MISC

--- src/App.js
+++ src/App.js
@@ -159,7 +159,7 @@ function App() {
               </div>
 
 
-              {questions.length > 0 && questions[0].difficulty == difficulty ?
+              {questions.length > 0 && questions[0].difficulty === difficulty ?
                 <button className="w-full text-white font-bold text-md uppercase bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 rounded-lg px-5 py-2.5 text-center mr-2 mb-2" onClick={startquestioncount}>
                   Start
                 </button> :
